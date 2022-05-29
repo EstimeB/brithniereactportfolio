7c5c2b49bc95cc86b94b5486abbbc42b614938f0
brith <bgc@briths-MacBook-Air.local>
2022-05-29 07:37:08 -0700

    Styling update (header.js).

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,25 +3,27 @@ import React from "react";
 // Using object destructuring assignment to pluck off variables from the props object
 function Header({ currentPage, handlePageChange }) {
   return (
-    <header>
-      <a
-        style={styles.a}
-        href="#home"
-        onClick={() => handlePageChange("Home")}
-        // Checks to see if Home is the current to set it to either active or not
-        className={
-          currentPage === "Home" ? "header-link active" : "header-link"
-        }
-      >
-        <h1 style={styles.tilteH1}>Brithnie Estime</h1>
-      </a>
-      <ul  style={styles.li}>
-        <li>
+    <header style={styles.header}>
+      <ul style={styles.li}>
+        {/* <li style={styles.li}> */}
+        <a
+          style={styles.titleA}
+          href="#home"
+          onClick={() => handlePageChange("Home")}
+          // Checks to see if Home is the current to set it to either active or not
+          className={
+            currentPage === "Home" ? "header-link active" : "header-link"
+          }
+        >
+          Brithnie Estime
+        </a>
+        {/* </li> */}
+        <li style={styles.li}>
           <a
             style={styles.a}
             href="#about"
             onClick={() => handlePageChange("About")}
-            // Checks to see if Home is the current to set it to either active or not
+            // Checks to see if About is the current to set it to either active or not
             className={
               currentPage === "About" ? "header-link active" : "header-link"
             }
@@ -29,12 +31,12 @@ function Header({ currentPage, handlePageChange }) {
             About
           </a>
         </li>
-        <li>
+        <li style={styles.li}>
           <a
             style={styles.a}
             href="#portfolio"
             onClick={() => handlePageChange("Portfolio")}
-            // Checks to see if Home is the current to set it to either active or not
+            // Checks to see if Portfolio is the current to set it to either active or not
             className={
               currentPage === "Portfolio" ? "header-link active" : "header-link"
             }
@@ -42,12 +44,12 @@ function Header({ currentPage, handlePageChange }) {
             Portfolio
           </a>
         </li>
-        <li>
+        <li style={styles.li}>
           <a
             style={styles.a}
             href="#contact"
             onClick={() => handlePageChange("Contact")}
-            // Checks to see if Home is the current to set it to either active or not
+            // Checks to see if Contact is the current to set it to either active or not
             className={
               currentPage === "Contact" ? "header-link active" : "header-link"
             }
@@ -55,7 +57,7 @@ function Header({ currentPage, handlePageChange }) {
             Contact
           </a>
         </li>
-        <li>
+        <li style={styles.li}>
           <a
             style={styles.a}
             href="#resume"
@@ -74,22 +76,25 @@ function Header({ currentPage, handlePageChange }) {
 }
 
 const styles = {
+  header: {
+    textAlign: "center",
+  },
   li: {
     listStyleType: "none",
-    display: "flex",
-    gap: "5px",
-    marginLeft: "300px",
-    marginRight: "30px",
+    display: "inline",
+    marginRight: "25px",
   },
   a: {
     textDecoration: "none",
     color: "black",
     fontSize: "20px",
-    marginLeft: "30px",
   },
-  titleH1: {
-    float: 'left',
-  }
+  titleA: {
+    textDecoration: "none",
+    fontSize: "40px",
+    color: "black",
+    marginRight: "25px",
+  },
 };
 
 export default Header;
